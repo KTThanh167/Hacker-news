bc76efe136c038f19d5b5dcc7e399c9c5c78d390
Thanh <ktthanh1673@gmail.com>
2026-04-14 14:05:34 +0700

    chore: remove unused view components and clean up router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,42 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import NewestView from '../views/NewestView.vue'
-import AskView from '../views/AskView.vue'
-import ShowView from '../views/ShowView.vue'
-import JobView from '../views/JobView.vue'
 
-import MainLayout from '@/Layout/MainLayout.vue'
+import MainLayout from '@/Layouts/MainLayout.vue'
 
 const routes = [
   {
     path: '/',
     component: MainLayout,
     children: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
-    // {
-    //   path: '/newest',
-    //   name: 'newest',
-    //   component: NewestView,
-    // },
-    // {
-    //   path: '/ask',
-    //   name: 'ask',
-    //   component: AskView,
-    // },
-    // {
-    //   path: '/show',
-    //   name: 'show',
-    //   component: ShowView,
-    // },
-    // {
-    //   path: '/jobs',
-    //   name: 'jobs',
-    //   component: JobView,
-    // },
     {
       path: '/:type', // Tham số động :type
       name: 'feed',
